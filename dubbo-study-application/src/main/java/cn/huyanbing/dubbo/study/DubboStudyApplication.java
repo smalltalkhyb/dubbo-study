f4d22fae593c4f8d61dad580a367ded2004be450
huyanbing <huyanbing@itbox.cn>
2024-10-21 14:44:47 +0800

    init project

---
+++ dubbo-study-application/src/main/java/cn/huyanbing/dubbo/study/DubboStudyApplication.java
@@ -0,0 +1,15 @@
+package cn.huyanbing.dubbo.study;
+
+/**
+ * @BelongProject dubbo-study
+ * @BelongPackage cn.huyanbing.dubbo.study
+ * @Description TODO
+ * @ClassName DubboStudyApplication
+ * @Author huyangbing
+ * @Date 2024/10/21 14:44
+ * @Version 1.0.0
+ */
+public class DubboStudyApplication {
+
+
+}
